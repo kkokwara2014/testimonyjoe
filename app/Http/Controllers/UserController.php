a07d2f9a379c22cb27f9cc8fa5e3d8d3216e2343
kkokwara2014 <kkokwara2014@gmail.com>
2020-01-18 09:10:14 +0100

    --- Auto-Git Commit ---

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Image;
+
+class UserController extends Controller
+{
+    public function profileimage()
+    {
+
+        return view('admin.user.profile', array('user' => Auth::user()));
+    }
+
+    public function updateprofileimage(Request $request)
+    {
+        $this->validate($request, [
+            'userimage' => 'required|image|mimes:jpg,jpeg,png|max:10000',
+        ]);
+
+        if ($request->hasFile('userimage')) {
+            $userimage = $request->file('userimage');
+            $filename = time() . '.' . $userimage->getClientOriginalExtension();
+            Image::make($userimage)->resize(300, 300)->save(public_path('user_images/' . $filename));
+
+            $user = Auth::user();
+            $user->userimage = $filename;
+            $user->save();
+        }
+
+        return view('admin.user.profile', array('user' => Auth::user()));
+    }
+}
