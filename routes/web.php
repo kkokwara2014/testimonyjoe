c5447a25ac97a48faf4d47d68bfa70b62f5857f2
kkokwara2014 <kkokwara2014@gmail.com>
2020-01-18 08:39:47 +0100

    --- Auto-Git Commit ---

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,3 +34,25 @@ Route::get('booking', 'FrontendController@booking')->name('booking');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'dashboard', 'middleware' => ['auth', 'admin']], function () {
+
+    Route::get('/', 'AdminController@index')->name('dashboard.index');
+    Route::resource('admin','AdminController');
+
+    Route::resource('category', 'CategoryController');
+    Route::resource('equipment', 'EquipmentController');
+    // Route::('contact', 'ContactController');getget
+    
+    Route::get('all/admins', 'AdminController@admins')->name('all.admin');
+    Route::get('admins/{id}/show', 'AdminController@show')->name('admins.show');
+    Route::post('admins/{id}/activate', 'AdminController@activate')->name('admins.activate');
+    Route::post('admins/{id}/deactivate', 'AdminController@deactivate')->name('admins.deactivate');
+
+    // Route::get('contact','ContactController@index')->name('contact.index');
+    // Route::delete('contact/{id}','ContactController@destroy')->name('contact.destroy');
+
+    Route::get('user/profile', 'UserController@profileimage')->name('user.profile');
+    Route::post('user/profile', 'UserController@updateprofileimage')->name('user.profile.update');
+});
