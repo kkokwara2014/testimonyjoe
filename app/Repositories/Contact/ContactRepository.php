927362c7f92c0ebb9412f1d673d4e0f3dbb4d1f2
kkokwara2014 <kkokwara2014@gmail.com>
2020-01-21 11:04:30 +0100

    --- Auto-Git Commit ---

--- app/Repositories/Contact/ContactRepository.php
+++ app/Repositories/Contact/ContactRepository.php
@@ -5,7 +5,8 @@ namespace App\Repositories\Contact;
 interface ContactRepository{
 
     public function getAll();
+    public function getById($id);
     public function create(array $data);
     public function delete($contactId);
-    
+
 }
--- app/Repositories/Contact/EloquentContact.php
+++ app/Repositories/Contact/EloquentContact.php
@@ -2,8 +2,29 @@
 
 namespace App\Repositories\Contact;
 
+use App\Contact;
 
 class EloquentContact implements ContactRepository
 {
-    
+    private $contact;
+
+    public function __construct(Contact $contact)
+    {
+        $this->contact=$contact;
+    }
+
+    public function getAll(){
+        return $this->contact->all();
+    }
+    public function getById($id){
+        return $this->contact->findById($id);
+    }
+
+    public function create(array $data){
+        return $this->contact->create($data);
+    }
+
+    public function delete($contactId){
+
+    }
 }
