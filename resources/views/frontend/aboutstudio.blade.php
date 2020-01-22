39939c467aade224645f49dc223ff9dc0681c7ac
kkokwara2014 <kkokwara2014@gmail.com>
2020-01-22 07:52:42 +0100

    --- Auto-Git Commit ---

--- resources/views/frontend/aboutstudio.blade.php
+++ resources/views/frontend/aboutstudio.blade.php
@@ -17,12 +17,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{route('index')}}"><i class="fa fa-home"></i> Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">@yield('title')</li>
-                    </ol>
-                </nav>
+
             </div>
         </div>
     </div>
