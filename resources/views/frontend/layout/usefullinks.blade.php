5aa25c61fe73f627a6c17dd0e282a6088022d9e7
kkokwara2014 <kkokwara2014@gmail.com>
2020-01-22 12:40:56 +0100

    --- Auto-Git Commit ---

--- resources/views/frontend/layout/usefullinks.blade.php
+++ resources/views/frontend/layout/usefullinks.blade.php
@@ -1,24 +1,24 @@
-<a href="#"><span class="fa fa-arrow-circle-o-right"></span> Home</a>
+<a href="{{route('index')}}"><span class="fa fa-arrow-circle-o-right"></span> Home</a>
 <br>
 
-<a href="#"><span class="fa fa-arrow-circle-o-right"></span> About Testimony Joe</a>
+<a href="{{route('abouttestimonyjoe')}}"><span class="fa fa-arrow-circle-o-right"></span> About Testimony Joe</a>
 <br>
 
-<a href="#"><span class="fa fa-arrow-circle-o-right"></span> About Holy Ghost Studio</a>
+<a href="{{route('aboutholyghoststudio')}}"><span class="fa fa-arrow-circle-o-right"></span> About Holy Ghost Studio</a>
 
 <br>
-<a href="#"><span class="fa fa-arrow-circle-o-right"></span> About Rentals</a>
+<a href="{{route('aboutrentals')}}"><span class="fa fa-arrow-circle-o-right"></span> About Rentals</a>
 <br>
-<a href="#"><span class="fa fa-arrow-circle-o-right"></span> Album</a>
+<a href="{{route('album')}}"><span class="fa fa-arrow-circle-o-right"></span> Album</a>
 
 <br>
-<a href="#"><span class="fa fa-arrow-circle-o-right"></span> Booking</a>
+<a href="{{route('booking')}}"><span class="fa fa-arrow-circle-o-right"></span> Booking</a>
 <br>
-<a href="#"><span class="fa fa-arrow-circle-o-right"></span> Event</a>
+<a href="{{route('event')}}"><span class="fa fa-arrow-circle-o-right"></span> Event</a>
 <br>
-<a href="#"><span class="fa fa-arrow-circle-o-right"></span> Gallery</a>
+<a href="{{route('gallery')}}"><span class="fa fa-arrow-circle-o-right"></span> Gallery</a>
 <br>
-<a href="#"><span class="fa fa-arrow-circle-o-right"></span> Contact</a>
+<a href="{{route('contact')}}"><span class="fa fa-arrow-circle-o-right"></span> Contact</a>
 <br>
 <br>
 
